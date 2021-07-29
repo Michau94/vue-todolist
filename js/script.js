118d7bb81a16ff6b6429253561e6ee943cf62d86
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-07-29 14:38:46 +0200

    no empty tasks + wmpty input after submit

--- js/script.js
+++ js/script.js
@@ -3,21 +3,29 @@ console.log(Vue);
 const root = new Vue({
     el: '#root',
     data: {
+        newTask: '',
         tasks: [
             'Sleep',
             'Eat',
             'Code',
             'Repeat'
-        ]
+        ],
 
     },
     methods: {
         removeTask(i) {
             this.tasks.splice(i, 1);
+        },
+
+        submitTask() {
+            if (this.newTask.trim() !== '') {
+
+                //submit new task dolo se non spazio vuoto
+                this.tasks.push(this.newTask);
+            }
+            // svuoto new task dopo submit
+            this.newTask = '';
         }
-
     }
-
-
 
 })
