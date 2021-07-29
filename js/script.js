99398679197b746f8582659a57a23aa4139afa14
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-07-29 14:12:08 +0200

    to doo list printed

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log(Vue);
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        tasks: [
+            'Sleep',
+            'Eat',
+            'Code',
+            'Repeat'
+        ]
+
+    },
+    methods: {
+
+    }
+
+
+
+})
